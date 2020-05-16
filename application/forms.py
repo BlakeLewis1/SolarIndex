d2186a4faa697f997419ed2a9b3160ec78cea21d
BlakeLewis1 <BLewis@academytrainee.com>
2020-05-16 20:30:12 +0000

    planets form

---
+++ application/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class PlanetForm(FlaskForm):
+    planet_name = StringField('Planet Name',
+        validators = [
+            DataRequired(),
+            Length(min=2, max=30)
+        ]
+    )
+    planet_type = StringField('Planet Type',
+        validators = [
+            DataRequired(),
+            Length(min=2, max=30)
+        ]
+    )
+    describe = StringField('Describe the Planet',
+        validators = [
+            DataRequired(),
+            Length(min=2, max=1000)
+        ]
+    )
+    submit = SubmitField('Add Planet')
--- application/routes.py
+++ application/routes.py
@@ -1,10 +1,31 @@
-from flask import render_template
-from application import app
+from flask import render_template, redirect, url_for
+from application import app, db
+from application.models import Planets
+from application.forms import PlanetForm
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', title='Home')
 
+ 
+@app.route('/planet', methods=['GET', 'POST'])
+def post():
+    form = PlanetForm()
+    if form.validate_on_submit():
+        planetData = Planets(
+            planet_name = form.planet_name.data,
+            planet_type = form.planet_type.data,
+            describe = form.describe.data
+        )
 
+        db.session.add(planetData)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    else:
+        print(form.errors)
+
+    return render_template('planet.html', title='add Planet', form=form)
 
