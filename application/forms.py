d853c08ba6284a40425625e13268b18b8b4a7690
BlakeLewis1 <BLewis@academytrainee.com>
2020-05-21 10:59:10 +0000

    crud features complete

--- application/forms.py
+++ application/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Length
 
 class PlanetForm(FlaskForm):
@@ -19,6 +19,11 @@ class PlanetForm(FlaskForm):
         validators = [
             DataRequired(),
             Length(min=2, max=1000)
+        ]
+    )
+    ssystem = IntegerField("solar system",
+        validators = [
+            DataRequired()
         ]
     )
     submit = SubmitField('Add Planet')
--- application/routes.py
+++ application/routes.py
@@ -15,13 +15,13 @@ def home():
 @app.route('/planet', methods=['GET', 'POST'])
 def planet():
     form = PlanetForm()
-    solar = 1
+    solar = Solar.query.all()
     if form.validate_on_submit():
         planetData = Planets(
             planet_name = form.planet_name.data,
             astronomical_type = form.astronomical_type.data,
             describe = form.describe.data,
-            ssystem_id = solar
+            ssystem_id = form.ssystem.data
         )
 
         db.session.add(planetData)
@@ -34,7 +34,7 @@ def planet():
         
 
 
-    return render_template('planet.html', title='Create the Planet', form=form)
+    return render_template('planet.html', title='Create the Planet', form=form, solars=solar)
 
 @app.route('/solar', methods=['GET', 'POST'])
 def solar():
@@ -53,19 +53,23 @@ def solar():
 
 @app.route('/<name>/update', methods=['GET', 'POST'])
 def updateplanet(name):
+    solar = Solar.query.all()
+
     planets = Planets.query.filter_by(planet_name=name).first()
     form = PlanetForm()
     if form.validate_on_submit():
         planets.planet_name=form.planet_name.data
         planets.astronomical_type=form.astronomical_type.data
         planets.describe=form.describe.data
+        planets.ssystem_id=form.ssystem.data
         db.session.commit()
         return redirect(url_for('home'))
     elif request.method == 'GET':
         form.planet_name.data = planets.planet_name
         form.astronomical_type.data=planets.astronomical_type
         form.describe.data=planets.describe
-    return render_template('planet.html', title='update planet', form=form)
+        form.ssystem.data=planets.ssystem_id
+    return render_template('planet.html', title='update planet', form=form, solars=solar)
 
 @app.route('/<name>/delete', methods=['GET', 'POST'])
 def deleteplanet(name):
@@ -73,7 +77,17 @@ def deleteplanet(name):
     for p in planet:
         db.session.delete(p)
 
-    
+        db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/home/<name>/delete', methods=['GET', 'POST'])
+def deletesolar(name):
+    solar = Solar.query.filter_by(id=name).first()
+    planets = Planets.query.filter_by(ssystem_id=name).all()
+    for planet in planets:
+        db.session.delete(planet) 
+    db.session.delete(solar)
+        
 
     db.session.commit()
     return redirect(url_for('home'))
